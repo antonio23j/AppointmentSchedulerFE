c81444f7ba38e3b9d0688f122d5fd3f8553e0cb6
antonio <antonio.jaicari1@gmail.com>
2023-10-24 23:31:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const data = new FormData(form);
+  // Convert FormData to an object
+  const formDataObject = {};
+  data.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+
+  // Convert the date field to ISO string
+  formDataObject.dateTime = new Date(formDataObject.dateTime).toISOString();
+
+
+  try {
+    const res = await fetch("http://localhost:8080/createAppointment", {
+      method: 'POST',
+      body: JSON.stringify(formDataObject),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    },
+    );
+    const resData = await res.text();
+    const divRes = document.getElementById('res');
+    divRes.innerHTML = resData;
+
+    console.log(resData)
+  } catch (err) {
+    console.log(err.message)
+  }
+});
---
+++ print.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form')
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const data = new FormData(form);
+    // Convert FormData to an object
+    const formDataObject = {};
+    data.forEach((value, key) => {
+        formDataObject[key] = value;
+        console.log(value);
+    });
+
+    //Convert the date field to ISO String
+    formDataObject.date = new Date(formDataObject.date).toISOString();
+    console.log(formDataObject.date);
+
+    try {
+
+        const res = await fetch("http://localhost:8080/getAppointmentsByDate?date=" + formDataObject.date, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const resData = await res.json();
+        const table = document.getElementById('tbody');
+        table.innerHTML = "";
+        resData.forEach((element) => {
+            const row = table.insertRow();
+            const cell1 = row.insertCell();
+            const cell2 = row.insertCell();
+            const cell3 = row.insertCell();
+            const cell4 = row.insertCell();
+            const cell5 = row.insertCell(); 
+            cell1.innerHTML = element['id'];
+            cell2.innerHTML = element['name'];
+            cell3.innerHTML = element['startDateTime'];
+            cell4.innerHTML = element['duration'];
+            cell5.innerHTML = element['endDateTime'];
+        })
+        console.log(resData);
+    } catch(err){
+        console.log(err.message);
+    }
+
+})
